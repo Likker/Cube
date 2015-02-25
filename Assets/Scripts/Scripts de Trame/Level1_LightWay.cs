055bdef723752eb8a6ffced2721dc22ac2f3b2b7
Maxime L <maxime.laffaire@epitech.eu>
2015-02-26 00:52:07 +0100

    Add room (not finished)
    Delete Prototype prefab : can't be used at prefab, use Prototype save
    and load option

---
+++ Assets/Scripts/Scripts de Trame/Level1_LightWay.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1_LightWay : MonoBehaviour {
+
+    public Transform[] positions;
+    public float _duration = 0.1f;
+    public float _timetoWait = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("followWay");	
+	}
+
+    IEnumerator followWay()
+    {
+        while (_timetoWait > 0.0f)
+        {
+            _timetoWait -= Time.deltaTime;
+            yield return 0;
+        }
+        foreach (Transform t in positions)
+        {
+            yield return StartCoroutine(goToPos(t));
+        }
+
+    }
+
+    IEnumerator goToPos(Transform t)
+    {
+        float startTime = Time.time;
+
+        while (!this.isNear(t.position, transform.position))
+        {
+            transform.position = Vector3.Lerp(transform.position, t.position, (Time.time - startTime) / (_duration * Time.deltaTime));
+            yield return 0;
+        }
+    }
+
+    bool isNear(Vector3 a, Vector3 b)
+    {
+        return (Mathf.Abs((a - b).magnitude) < 0.1f);
+    }
+}
---
+++ Assets/Scripts/randomLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomLight : MonoBehaviour {
+
+    public float minIntensity;
+    public float maxIntensity;
+
+    float random;
+
+    void Start()
+    {
+        random = Random.Range(0.0f, 65535.0f);
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time);
+        light.range = Mathf.Lerp(minIntensity, maxIntensity, noise);
+    }
+}
