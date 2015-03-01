e8fdd33078f16ef8f5bad6a6861255225134aceb
Maxime L <maxime.laffaire@epitech.eu>
2015-03-01 22:20:57 +0100

    Some changes added, begin save

--- Assets/Scripts/Scripts de Trame/MainRoom.cs
+++ Assets/Scripts/Scripts de Trame/MainRoom.cs
@@ -5,10 +5,13 @@ public class MainRoom : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        WritingEvent.instance.setText("Introduction");
-        WritingEvent.instance.setText("IntroductionTwo");
-        WritingEvent.instance.setText("IntroductionThree");
-        WritingEvent.instance.setText("FirstOrder");
+        if (!PlayerPrefs.HasKey("notFirstGame"))
+        {
+            WritingEvent.instance.setText("Introduction");
+            WritingEvent.instance.setText("IntroductionTwo");
+            WritingEvent.instance.setText("IntroductionThree");
+            WritingEvent.instance.setText("FirstOrder");
+        }
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Scripts de Trame/MainRoom_Portal.cs
+++ Assets/Scripts/Scripts de Trame/MainRoom_Portal.cs
@@ -12,6 +12,8 @@ public class MainRoom_Portal : MonoBehaviour {
 
     public float speedToAppear = 400.0f;
 
+    private float reset = 0.0f;
+
 	// Use this for initialization
 	void Start () {
         foreach (Text t in textToAppear)
@@ -23,7 +25,16 @@ public class MainRoom_Portal : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKey(KeyCode.R))
+            reset += Time.deltaTime;
+        else
+            reset = 0.0f;
+        if (reset >= 7.0f)
+        {
+            PlayerPrefs.DeleteAll();
+            WritingEvent.instance.setText("MessageReset");
+            reset = 0.0f;
+        }
 	}
 
     IEnumerator DownPortal()
@@ -54,6 +65,6 @@ public class MainRoom_Portal : MonoBehaviour {
             yield return 0;
         }
 
-
+        PlayerPrefs.SetInt("notFirstGame", 1);
     }
 }
