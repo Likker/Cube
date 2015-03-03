fa13f91e622953f881c9114c89d468b040875c93
Paul-Auguste Lacave <paulcmoi51@gmail.com>
2015-03-03 20:24:50 +0100

    Add modif level 3

--- Assets/Scripts/MoveY.cs
+++ Assets/Scripts/MoveY.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class MoveY : MonoBehaviour {
 
 	// Use this for initialization
+    public bool AR = true;
     public float y = 2;
     public double speed = 1;
 
     // Use this for initialization
     void Start()
     {
-        iTween.MoveBy(gameObject, iTween.Hash("y", y, "easeType", "linear", "loopType", "pingPong", "speed", speed));
+        if (AR)
+            iTween.MoveBy(gameObject, iTween.Hash("y", y, "easeType", "linear", "loopType", "pingPong", "speed", speed));
+        else
+            iTween.MoveTo(gameObject, iTween.Hash("y", y, "easeType", "linear", "speed", speed));
     }
 	
 	// Update is called once per frame
