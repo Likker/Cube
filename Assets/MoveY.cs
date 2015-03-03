f7f5f288faa03f1cf31d9880f8f21c3482224c52
Paul-Auguste Lacave <paulcmoi51@gmail.com>
2015-03-03 18:39:11 +0100

    Level 3 Modif

---
+++ Assets/MoveY.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveY : MonoBehaviour {
+
+	// Use this for initialization
+    public float y = 2;
+    public double speed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        iTween.MoveBy(gameObject, iTween.Hash("y", y, "easeType", "linear", "loopType", "pingPong", "speed", speed));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
